fdd9411b4b2f670b4573101579cd9b52cfa231e9
viktorijagardauskiene <viktorija.gardauskiene@gmail.com>
2017-07-12 19:36:32 +0300

    iesko masyve ar yra tokia reiksme ir kurioje pozicijoje jis yra

---
+++ uzduotys.php
@@ -0,0 +1,61 @@
+
+<pre>
+<?php
+$b = ["zole", "pienas", "karve"];
+$a=[5, 8, 4, 6, 3, 1];
+$c = "orangutangas";
+$x = 3;
+
+
+ if (array_search($x, $a)) {
+ 	echo "Yra";
+ } else {
+ 	echo "Nera";
+ }
+
+echo "<br />";
+
+$position_in_array = array_search($x, $a);
+
+if ($position_in_array != false) {
+	echo $position_in_array;
+} else {
+	echo "elementas nerastas";
+}
+
+
+/* //$d = 1;
+//if(isset($d)) {
+//	unset($d);
+//	echo($d);
+//}
+
+echo "<br />";
+
+$cc = str_split($c);
+
+print_r($a);
+
+
+
+shuffle($a);
+print_r($a);
+
+
+sort($b);
+print_r($b);
+
+shuffle($b);
+print_r($b);
+
+
+print_r($c);
+echo "<br />";
+sort($cc);
+print_r($cc);
+
+shuffle($cc);
+print_r($cc);
+*/
+?>
+</pre>
